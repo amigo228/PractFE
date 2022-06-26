0d10f7659dcaa45c1e0f57488e8a110d7774c895
Rostyslav Rudyi <amiguys9@email.com>
2022-06-26 15:47:57 +0200

    latest news

---
+++ src/js/main.js
@@ -0,0 +1,26 @@
+$('.latest-slider').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    dots: true,
+
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                arrows: false,
+                centerMode: true,
+                centerPadding: '40px',
+                slidesToShow: 2
+            }
+        },
+        {
+            breakpoint: 768,
+            settings: {
+                arrows: false,
+                centerMode: true,
+                centerPadding: '40px',
+                slidesToShow: 1
+            }
+        }
+    ]
+});
